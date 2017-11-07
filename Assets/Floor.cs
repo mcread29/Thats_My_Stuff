c55129c21f290289b9457e708c0b6581f932db74
Benjamin James Hallsted <bhallste@crm.ucsc.edu>
2017-11-07 09:44:22 -0800

    Add spikes with cooldown; No spawning for the spikes. but they deal damage

--- Assets/Floor.cs
+++ Assets/Floor.cs
@@ -6,16 +6,27 @@ public class Floor : MonoBehaviour {
 
 	public float damage;
 	public float fireCooldown;
-	
-	
+	public float range;
 
+	private BoxCollider col;
+	
 	// Use this for initialization
 	void Start () {
-		
+		col = GetComponent<BoxCollider> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (fireCooldown > 0){
+			fireCooldown -= 1;
+		}
 	}
+
+	void OnTriggerStay(Collider other){
+		if (fireCooldown <= 0) {
+			Destroy (other.gameObject);
+			fireCooldown = 100;
+		} 
+	}
+	
 }
